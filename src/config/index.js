35fc9160e34d5032e5b1f5c21c956d55af2fe1ba
洪阳林 <yanglin.hong@huilianyi.com>
2021-11-25 19:46:43 +0800

    fix.

--- src/config/index.js
+++ src/config/index.js
@@ -3,45 +3,59 @@
  * */
 import glob from "glob";
 import path from "path";
-import { FILE_TYPE } from "../stream/enum/index.js";
+import {FILE_TYPE} from "../stream/enum/index.js";
+import fs from "file-system";
 
-const runPath = path.resolve("");
-const projectName = "multi-language";
+let runPath = path.resolve("");
+const projectName = "multi-language-tools";
+let transitionPath = '';
+if (!runPath.includes(projectName)) {
+    for (let i = 0; i < 4; i++) {
+        const files = fs.readdirSync(runPath);
+        if (!files.include('node_modules')) {
+            break;
+        }
+        const pathList = runPath.split("/");
+        pathList.pop();
+        runPath = pathList.join('/');
+    }
+    transitionPath = "/node_modules/";
+}
 const configPath =
-  runPath.split(projectName)[0] + projectName + "/src/config/*.js";
+    runPath.split(projectName)[0] + transitionPath + projectName + "/src/config/*.js";
 
 let combineConfig = {};
 const load = (moduleName) => {
-  moduleName = path.resolve(moduleName);
-  /*Unfortunately, synchronized dynamic import is not supported, such as::import.meta.glob*/
-  return import(moduleName);
+    moduleName = path.resolve(moduleName);
+    /*Unfortunately, synchronized dynamic import is not supported, such as::import.meta.glob*/
+    return import(moduleName);
 };
 let moduleList = [];
 glob.sync(configPath).forEach((filePath) => {
-  const fileExtension = filePath.split(".").pop().toLowerCase();
-  let fileName = filePath.split("/").pop();
-  if (FILE_TYPE.JS === fileExtension) {
-    const currentFileName = "index.js";
-    if (currentFileName != fileName) {
-      const _load = load(filePath);
-      moduleList.push(_load);
+    const fileExtension = filePath.split(".").pop().toLowerCase();
+    let fileName = filePath.split("/").pop();
+    if (FILE_TYPE.JS === fileExtension) {
+        const currentFileName = "index.js";
+        if (currentFileName != fileName) {
+            const _load = load(filePath);
+            moduleList.push(_load);
+        }
     }
-  }
 });
 Promise.all(moduleList).then((res) => {
-  res.forEach((item) => {
-    if (!Object.keys(combineConfig).length) {
-      combineConfig = item.default;
-    } else {
-      Object.keys(item.default).forEach((attr) => {
-        if (combineConfig.hasOwnProperty(attr)) {
-          console.error(`Duplicate module name, please modify ${attr}.`);
-          return;
+    res.forEach((item) => {
+        if (!Object.keys(combineConfig).length) {
+            combineConfig = item.default;
         } else {
-          combineConfig[attr] = item[attr];
+            Object.keys(item.default).forEach((attr) => {
+                if (combineConfig.hasOwnProperty(attr)) {
+                    console.error(`Duplicate module name, please modify ${attr}.`);
+                    return;
+                } else {
+                    combineConfig[attr] = item[attr];
+                }
+            });
         }
-      });
-    }
-  });
+    });
 });
-export { combineConfig };
+export {combineConfig};
--- src/index.js
+++ src/index.js
@@ -2,34 +2,41 @@
 /*
   Copyright Enjoy Wind and other contributors, https://github.com/enjoy-wind
  */
-import { combineConfig } from "./config/index.js";
-import { parse } from "./parser/index.js";
-import { generator } from "./generator/index";
-import { projectIntegrationHooks } from "./hooks/index.js";
+import {combineConfig} from "./config/index.js";
+import {parse} from "./parser/index.js";
+import {generator} from "./generator/index.js";
+import {projectIntegrationHooks} from "./hooks/index.js";
+import {getChildFilesPath} from "./stream/index.js";
 
 const start = (config = {}, callBackHooks) => {
-  init().then((res) => {
-    Object.assign(combineConfig, config);
-    const { entryPath } = combineConfig;
-    const tokens = parse(entryPath);
-    generator(tokens).then((fullTokens) => {
-      projectIntegrationHooks(fullTokens);
-      if (callBackHooks) {
-        callBackHooks(fullTokens);
-      }
+    init().then((res) => {
+        Object.assign(combineConfig, config);
+        const {entryPath} = combineConfig;
+        const fileList = getChildFilesPath(entryPath);
+        let tokens = [];
+        fileList.forEach(item => {
+            tokens = tokens.concat(parse(item));
+        });
+        generator(tokens).then((fullTokens) => {
+            projectIntegrationHooks(fullTokens);
+            if (callBackHooks) {
+                callBackHooks(fullTokens);
+            }
+        });
     });
-  });
 };
 
 const init = () => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      const { version } = combineConfig;
-      console.log(`init load finish version:${version}.`);
-      resolve(true);
-    }, 10);
-  });
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            const {version} = combineConfig;
+            console.log(`init load finish version:${version}.`);
+            setTimeout(() => {
+                resolve(true);
+            }, 100)
+        }, 10);
+    });
 };
 start();
 
-export { start };
+export {start};
